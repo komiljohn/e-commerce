542df9e43f769bc6f811dd4612828143c346b941
Sardor Sayidaxmedov <sardor.sayidakhmedov@gmail.com>
2023-03-22 12:57:52 +0500

    feat: auto import scss files

--- i18n.js
+++ i18n.js
@@ -1,11 +1,12 @@
 module.exports = {
   locales: ['ru', 'en', 'uz'],
   defaultLocale: 'ru',
+  defaultNS: 'common',
   loadLocaleFrom: (lang, ns) =>
     import(`@/locales/${lang}/${ns}.json`).then((m) => m.default),
   pages: {
     '*': ['common'],
-    '/about': ['about']
+    '/about': ['about'],
   },
-  localeDetection: false
+  localeDetection: false,
 }
--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
+const path = require('path')
 
 const nextConfig = nextTranslate({
   reactStrictMode: true,
   images: {
-    domains: ['test.cdn.rasta.app']
-  }
+    domains: ['test.cdn.rasta.app'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `
+      @import "./src/styles/breakpoints.scss";
+      @import "./src/styles/mixins.scss";
+      @import "./src/styles/unit.scss";
+      `,
+  },
 })
 
 module.exports = nextConfig
